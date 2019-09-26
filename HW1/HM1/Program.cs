0c2e7e30fc1bb9edd2c5bd8cf3c70cd0d77751bd
AnnaMilevich <55656707+AnnaMilevich@users.noreply.github.com>
2019-09-26 10:53:56 +0300

    Add files via upload

---
+++ HW1/HM1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace HM1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            int sizeStr, maxCount, count;
+            maxCount = 0;
+
+            Console.WriteLine("Enter the word: ");
+            str = Console.ReadLine();
+            
+            sizeStr = str.Length;
+
+            for (int i = 0; i < sizeStr; i++)
+            {
+                count = 1;
+                for (int j = i + 1; j < sizeStr; j++)
+                {
+                    if (str[i] == str[j])
+                    {
+                        count += 1;
+                    }
+                }
+                if (count >= maxCount)
+                {
+                    maxCount = count;
+                }
+            }
+            Console.WriteLine(maxCount);
+            
+
+        }
+    }
+}
+
