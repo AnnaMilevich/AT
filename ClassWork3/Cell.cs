b73ac8fb02dc3b3acfb204372e292b9487c05cdf
AnnaMilevich <55656707+AnnaMilevich@users.noreply.github.com>
2019-11-05 18:31:32 +0300

    Add files via upload

---
+++ ClassWork3/Cell.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classwork3
+{
+    class Cell
+    {
+        char colum;
+        int row;
+        
+        public Cell(char colum, int row)
+        {
+
+          
+            // if(char.TryParse(columtest,out colum) && )
+            if (row < 1 || row > 8 || (int)colum < 97 || (int)colum > 104)
+            {
+                throw new OutRowOrColumBoard();
+            }
+            this.colum = (char)colum;
+            this.row = row;
+          
+        }
+    }
+}
---
+++ ClassWork3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classwork3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Cell cell1 = new Cell('h', 5);
+                Cell cell2 = new Cell('a', 15);
+                Cell cell3 = new Cell('m', 2);
+
+            }
+            catch (OutRowOrColumBoard ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
